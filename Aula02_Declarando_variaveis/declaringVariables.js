6ff83d4eda70175d64c22a0eb7af36fed16dea48
kellymourat <kellymoura.pt@gmail.com>
2025-07-04 17:45:54 +0100

    exercise variables

---
+++ Aula02_Declarando_variaveis/declaringVariables.js
@@ -0,0 +1,15 @@
+// let const 
+
+const primeiroValor = 20;
+const segundoValor = 30;
+
+console.log("primeiro + segundoValor:", primeiroValor + segundoValor );
+
+const valorTotal = primeiroValor + segundoValor;
+console.log( "valorTotal", valorTotal);
+
+let terceiroValor = 500;
+console.log("terceiroValor", terceiroValor);
+
+terceiroValor = 100;
+console.log("terceiroValor final: ", terceiroValor);
