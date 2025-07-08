98d5f57d86b5bdd11b129926af0bef9162aca0f9
kellymourat <kellymoura.pt@gmail.com>
2025-07-08 14:52:49 +0100

    feat: add arithmetic operators execise

---
+++ Aula04-Operadores/arithmeticOperators.js
@@ -0,0 +1,46 @@
+// Operadores aritiméticos
+// Opradores de atribuição
+// Operadores de comparação
+// Operadores lógicos 
+
+// salário
+
+const salario = 5000;
+
+const valeAlimentacao = 500;
+
+console.log("O salário total (incluindo VA) é: ", salario + valeAlimentacao);
+
+const salarioBruto = 7000;
+
+const descontos = 2000;
+
+console.log("Sálario que cai na conta é :", salarioBruto - descontos);
+
+console.log("_______________________________________________________");
+
+// valor do Buffet 
+
+const valorBuffetPorPessoa = 80;
+const totalConvidados = 100;
+
+console.log("O valor total do buffet é: ", valorBuffetPorPessoa * totalConvidados);
+
+const custoTotalCasamento = 80000;
+
+console.log("Custo por convidado: ", custoTotalCasamento / totalConvidados);
+
+console.log("_______________________________________________________");
+
+// números pares e ímpares (módulo)
+
+let numero = 10 ;
+console.log("O resto da divisão de " + numero + " por 2 é:", numero % 2);
+console.log("O número " + numero + " é PAR");
+
+numero = 11;
+console.log("O resto da divisão de " + numero + " por 2 é:", numero % 2);
+console.log("O número é ÍMPAR ");
+
+
+
