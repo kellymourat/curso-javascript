20656ccdd37e55c0aeeddc57f021b8a4274b242d
kellymourat <kellymoura.pt@gmail.com>
2025-07-08 15:16:20 +0100

    create assignment operators exercise

---
+++ Aula04-Operadores/assignmentOperators.js
@@ -0,0 +1,15 @@
+// Operador de atribuição (=)
+// Além do operador de atribuição simples, existem também operadores de atribuição combinados, como +=, -=, *=, /=, que realizam uma operação aritmética antes de atribuir o resultado. 
+
+let salarioAbril = 5000;
+
+// salarioAbril = salarioAbril + 3000;
+salarioAbril += 3000;
+console.log("O salário líquido de Abril é: ", salarioAbril);
+
+let salarioMaio = 7000;
+
+//salarioMaio = salarioMaio - 2000;
+salarioMaio -= 2000;
+console.log("O salário líquido de Maio é: ", salarioMaio);
+
