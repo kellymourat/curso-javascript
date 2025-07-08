37b6508bfc563c988662961bb9103c37b8ebe6d1
kellymourat <kellymoura.pt@gmail.com>
2025-07-08 11:30:32 +0100

    object exercises

--- Aula03_Tipos_de_dados/dataTypesObject.js
+++ Aula03_Tipos_de_dados/dataTypesObject.js
@@ -24,8 +24,11 @@ console.log('Idade', fichaDaKelly.idade); // retorna o valo undefined.
 
 
 console.log("Olá, " + fichaDaKelly.nome); // Sem interpolação (tradicional):
+
 console.log(`profissão: ${fichaDaKelly.profissao}`) //template literal, permite interpolar variáveis com ${...}.
+
 console.log(`Olá, ${fichaDaKelly.nome}`); // Mais limpo, mais fácil de ler e escrever.
 
 console.log('A' + 'B');
+
 console.log('A' + 2); // concatenação de strings é o processo de combinar duas ou mais strings em uma única string. O operador "+" é frequentemente usado para concatenar strings, ou seja - no console imprime AB.
